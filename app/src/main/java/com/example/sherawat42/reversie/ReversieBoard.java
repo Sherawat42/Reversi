eb3cb48bb71ef38936fb068f182717383198946f
sherawat42 <manishsherawat1@gmail.com>
2017-01-29 00:26:02 +0530

    testing

--- app/src/main/java/com/example/sherawat42/reversie/ReversieBoard.java
+++ app/src/main/java/com/example/sherawat42/reversie/ReversieBoard.java
@@ -7,13 +7,20 @@ import android.widget.LinearLayout;
  * Created by sherawat42 on 28/1/17.
  */
 
-public final class ReversieBoard {
-    public ReversieBoard(int dimensionX,int dimensionY,Context context){
+public final class ReversieBoard extends LinearLayout{
+    public ReversieBoard(Context context, int dimensionX,int dimensionY){
+        super(context);
+
+        boardRows = new LinearLayout[dimensionX];
         gameButtons = new ReversieButton[dimensionX][dimensionY];
-        LinearLayout boardRows;
         for(int i=0;i<dimensionX;i++){
+            boardRows[i] = new LinearLayout(context);
             gameButtons[i] = new ReversieButton[dimensionY];
         }
     }
-    ReversieButton[][] gameButtons;
+    private ReversieButton[][] gameButtons;
+    public ReversieButton getButton(int x, int y){
+        return gameButtons[x][y];
+    }
+    private LinearLayout boardRows[];
 }
---
+++ app/src/main/java/com/example/sherawat42/reversie/temp.java
@@ -0,0 +1,14 @@
+package com.example.sherawat42.reversie;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+/**
+ * Created by sherawat42 on 29/1/17.
+ */
+
+public class temp extends LinearLayout {
+    public temp(Context context, int X, int Y) {
+        super(context);
+    }
+}
