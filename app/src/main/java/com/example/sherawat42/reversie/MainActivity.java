54e597a92481575ff18199d4ec719221a2c1c01f
sherawat42 <manishsherawat1@gmail.com>
2017-01-29 02:27:20 +0530

    layout_done

--- app/src/main/java/com/example/sherawat42/reversie/MainActivity.java
+++ app/src/main/java/com/example/sherawat42/reversie/MainActivity.java
@@ -2,12 +2,60 @@ package com.example.sherawat42.reversie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    LinearLayout mainLayout;
+    private int dimensionX=8;
+    private int dimensionY=8;
+    private int p1Score=0;
+    private int p2Score=0;
+    TextView p1ScoreTV;
+    TextView p2ScoreTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainLayout = (LinearLayout)findViewById(R.id.activity_main);
+        setBoard();
+    }
+
+    private void setBoard() {
+        mainLayout.removeAllViews();
+
+        LinearLayout.LayoutParams params;
+
+        LinearLayout scoreBoard = new LinearLayout(this);
+        scoreBoard.setOrientation(LinearLayout.HORIZONTAL);
+        TextView textP1 = new TextView(this);
+        textP1.setText("PLAYER1: ");
+        TextView textP2 = new TextView(this);
+        TextView space = new TextView(this);
+        space.setText("      ");
+        textP2.setText("PLAYER2: ");
+        p1ScoreTV = new TextView(this);
+        p1ScoreTV.setText("0");
+        p2ScoreTV = new TextView(this);
+        p2ScoreTV.setText("0");
+        scoreBoard.addView(textP1);
+        scoreBoard.addView(p1ScoreTV);
+        scoreBoard.addView(space);
+        scoreBoard.addView(textP2);
+        scoreBoard.addView(p2ScoreTV);
+        params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1);
+        scoreBoard.setLayoutParams(params);
+        mainLayout.addView(scoreBoard);
+
+        ReversieBoard gameBoard = new ReversieBoard(this, dimensionX, dimensionY);
+        params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 3);
+        gameBoard.setLayoutParams(params);
+        mainLayout.addView(gameBoard);
     }
 }
--- app/src/main/java/com/example/sherawat42/reversie/ReversieBoard.java
+++ app/src/main/java/com/example/sherawat42/reversie/ReversieBoard.java
@@ -1,6 +1,7 @@
 package com.example.sherawat42.reversie;
 
 import android.content.Context;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 /**
@@ -8,19 +9,34 @@ import android.widget.LinearLayout;
  */
 
 public final class ReversieBoard extends LinearLayout{
-    public ReversieBoard(Context context, int dimensionX,int dimensionY){
+    public ReversieBoard(Context context, int dimensionX, int dimensionY){
         super(context);
-
-        boardRows = new LinearLayout[dimensionX];
-        gameButtons = new ReversieButton[dimensionX][dimensionY];
-        for(int i=0;i<dimensionX;i++){
-            boardRows[i] = new LinearLayout(context);
-            gameButtons[i] = new ReversieButton[dimensionY];
+        this.setOrientation(VERTICAL);
+        rows = new LinearLayout[dimensionX];
+        buttons = new ReversieButton[dimensionX][dimensionY];
+        LayoutParams paramsRow = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1);
+//        paramsRow.setMargins(2,0,0,2);
+        LayoutParams paramsButton = new LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT,1);
+//        paramsButton.setMargins(2,2,2,2);
+        for(int i=0; i<dimensionX;i++){
+            rows[i] = new LinearLayout(context);
+            rows[i].setLayoutParams(paramsRow);
+            rows[i].setOrientation(HORIZONTAL);
+            for(int j=0;j<dimensionY;j++){
+                buttons[i][j] = new ReversieButton(context);
+                buttons[i][j].setPlayer(0);
+                buttons[i][j].setText("");
+                buttons[i][j].setLayoutParams(paramsButton);
+                rows[i].addView(buttons[i][j]);
+            }
+            this.addView(rows[i]);  // TRY writtig this line at the starting of the above for loop
         }
+
     }
-    private ReversieButton[][] gameButtons;
-    public ReversieButton getButton(int x, int y){
-        return gameButtons[x][y];
+    private ReversieButton[][] buttons;
+    private LinearLayout[] rows;
+
+    public ReversieButton getButtons(int x, int y) {
+        return buttons[x][y];
     }
-    private LinearLayout boardRows[];
 }
